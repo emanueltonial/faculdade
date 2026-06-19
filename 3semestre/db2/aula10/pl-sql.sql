69fefdc5595cfc94f3835c3f969beb04e1ccbc82
manutonial <manumanusi2014@gmail.com>
2026-06-18 21:57:38 -0300

    aula bd2 + trab ead 18/06

---
+++ 3semestre/db2/aula10/pl-sql.sql
@@ -0,0 +1,46 @@
+select trunc(dt_emis, 'MM') Emissao, sum(peso_brt) PesoBruto
+from tnfs_saida_hist
+where dt_emis between '30-NOV-02' and '30-NOV-04' 
+and uf = 'RS' 
+and sit_nf = 'I' 
+and peso_liq > 100000
+group by trunc(dt_emis, 'MM');
+
+/* Criação do Index */
+create index tnfs_saida_hist_ix1
+on tnfs_saida_hist (dt_emis, uf, sit_nf, peso_liq);
+
+/* SELECTs*/
+
+select uf, count(*)
+from tnfs_saida_hist
+group by uf order by 2;
+
+select count(*)
+from tnfs_saida_hist
+where uf = 'RS';
+
+select uf, cidade, sum(vlr_total)
+from tnfs_saida_hist
+where dt_emis between '01-MAR-04' and '31-MAR-04'
+and sit_nf = 'C'
+and vlr_total > 0
+group by uf, cidade
+having sum(vlr_total) > 1000000;
+
+select dt_emis,
+sum(vlr_total)VlrTotal,
+sum(peso_liq)PesoLiq,
+sum(peso_brt)PesoBrt
+from tnfs_saida_hist
+where dt_emis between '01-JAN-04' and '10-JAN-04'
+group by dt_emis
+order by 1;
+
+select dt_emis,
+sum(vlr_total)VlrTotal,
+sum(peso_liq)PesoLiq
+from tnfs_saida_hist
+where dt_emis between '01-JAN-04' and '10-JAN-04'
+group by dt_emis
+order by 1;
---
+++ 3semestre/db2/trabalhos-semestre/no-sql-db/bancos-de-dados-nosql.md
@@ -0,0 +1,4 @@
+# Atividade EAD BD2 - Bancos de Dados NoSQL
+
+## 1) Os bancos de dados NoSQL armazenam informações não-estruturadas, ao contrário dos bancos de dados relacionais onde as informações estão estruturadas. Mas o que significa “estruturar” as informações? o que seria uma “estrutura” ?
+
