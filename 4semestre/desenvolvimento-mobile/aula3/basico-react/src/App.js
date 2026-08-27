78f0eb601945df3975b0bca9e615147e9a1075ae
emanueltonial <emanueltonial77@outlook.com>
2026-08-26 21:47:31 -0300

    add aula 5 poo2 pt1

--- 4semestre/desenvolvimento-mobile/aula3/basico-react/src/App.js
+++ 4semestre/desenvolvimento-mobile/aula3/basico-react/src/App.js
@@ -8,7 +8,7 @@ export default function App() {
     <div className="App">
       <h1>Hello {x}</h1>
       <Botao nome="Bom dia" cor="red" />
-      <Botao nome="Boa tarde" cor="blue" />
+      <Botao nome="Boa tarde" cowr="blue" />
       <Botao nome="Boa noite" cor="green" />
       <Botao nome="Boa madrugada" cor="black" />
     </div>
