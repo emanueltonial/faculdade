ae247dfb56e236c460f761505e7a443509620cba
emanueltonial <emanueltonial77@outlook.com>
2026-09-22 22:17:43 -0300

    add aula 7 redes-e-sistemas-distribuidos

---
+++ 4semestre/redes-e-sistemas-distribuidos/aula7/ServidorMultiplo/src/servidor/ConexaoCliente.java
@@ -0,0 +1,36 @@
+package servidor;
+
+import java.io.*;
+import java.net.*;
+
+public class ConexaoCliente implements Runnable {
+    private Socket minhaConexao;
+    private PrintStream msgSaida;
+    private BufferedReader msgEntrada;
+
+    public ConexaoCliente(Socket conexao) {
+        if(conexao == null) {
+            throw new IllegalArgumentException("Voce forneceu uma conexao invalida");
+        }
+        minhaConexao = conexao;
+    }
+
+    public void run() {
+        try {
+            msgSaida = new PrintStream(minhaConexao.getOutputStream());
+            msgEntrada = new BufferedReader(new InputStreamReader(
+                minhaConexao.getInputStream()
+            ));
+
+            String mensagem = msgEntrada.readLine();
+            while(mensagem != null && !mensagem.equals("FIM")) {
+                System.out.println(mensagem);
+                msgSaida.println("Positivo e Operante");
+                mensagem = msgEntrada.readLine();
+            }
+            minhaConexao.close();
+        } catch(IOException exc) {
+            exc.printStackTrace();
+        }
+    }
+}
